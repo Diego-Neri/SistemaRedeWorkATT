c0e397ecd30bdfe792b5ce16fb0d195f300ecb6b
Diego Neri <+Diego-Neri@users.noreply.github.com>
2024-10-08 19:38:55 -0300

    Feat: tela de vagas faltando correção

---
+++ Controllers/VagaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaRedeWork.Controllers {
+    public class VagaController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        public IActionResult VagaCompleta() {
+            return View();
+        }
+    }
+}
