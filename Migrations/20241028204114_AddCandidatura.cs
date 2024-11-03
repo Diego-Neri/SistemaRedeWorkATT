d41209f953638ad9827bf81e108dbdf0c6743b87
Diego Neri <+Diego-Neri@users.noreply.github.com>
2024-11-02 21:05:10 -0300

    att

---
+++ Migrations/20241028204114_AddCandidatura.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaRedeWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCandidatura : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidaturas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    VagaId = table.Column<int>(type: "int", nullable: false),
+                    EstudanteId = table.Column<int>(type: "int", nullable: false),
+                    DataCandidatura = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidaturas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Candidaturas_Estudantes_EstudanteId",
+                        column: x => x.EstudanteId,
+                        principalTable: "Estudantes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Candidaturas_Vagas_VagaId",
+                        column: x => x.VagaId,
+                        principalTable: "Vagas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidaturas_EstudanteId",
+                table: "Candidaturas",
+                column: "EstudanteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidaturas_VagaId",
+                table: "Candidaturas",
+                column: "VagaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidaturas");
+        }
+    }
+}
---
+++ Models/CandidaturaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaRedeWork.Models {
+    public class CandidaturaModel {
+        public int Id { get; set; }
+        public int VagaId { get; set; }
+        public CadastrarVagasModel Vagas { get; set; }
+        public int EstudanteId {  get; set; }
+        public EstudanteModel Estudante { get; set; }
+        public DateTime DataCandidatura { get; set; }
+    }
+}
